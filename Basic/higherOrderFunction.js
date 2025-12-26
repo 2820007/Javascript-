8781f4a78d6f8af821638aa615e3407179eb51f2
Ravi Mandal <2820007.cse@pietgroup.co.in>
2025-12-26 17:43:00 +0545

    higherorder function comleted

---
+++ Basic/higherOrderFunction.js
@@ -0,0 +1,44 @@
+//forEach replacement of for loop to accessing element of an array with index no return value.
+
+let arr=[1,2,3,4,5,67,4]
+
+// arr.forEach((item,index)=>{
+//     console.log(item+ "-"+index)
+// })
+
+//map  -New array return 
+//modify existing array and return an new array
+
+// const newArray=arr.map((item)=>{
+//     return item*2
+// })
+// console.log(newArray)
+
+
+//filter to filter existing array
+// const newArray=arr.filter((item)=>{
+//     return item%2==0
+
+// })
+// console.log(newArray)
+
+// Reduce to return in total value -
+
+// let totalVal=arr.reduce((acc,item)=>{
+//     return acc+item
+
+// },0)
+// console.log(totalVal)
+
+
+//find
+
+// let i=arr.find((item)=>{
+//     return item==4
+// })
+// console.log(i)
+//
+
+
+//includes
+// console.log(arr.includes(3))
