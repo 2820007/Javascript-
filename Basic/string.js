730d1a511075e26c886cb19776297441cf26d23a
Ravi Mandal <2820007.cse@pietgroup.co.in>
2025-12-30 17:49:39 +0545

    string competed

--- Basic/string.js
+++ Basic/string.js
@@ -1,4 +1,4 @@
-let fullName=" ravi mandal "
+let fullName="ravi mandal"
 // console.log(fullName)
 // console.log(fullName[0])
 // console.log(fullName[1])
@@ -10,8 +10,8 @@ let fullName=" ravi mandal "
 // }
 
 
-let firstName="ravi"
-let middleName="Kumar"
+// let firstName="ravi"
+// let middleName="Kumar"
 
 // console.log(firstName+ " "+ middleName)
 // console.log(firstName.concat(" " +middleName))
@@ -24,5 +24,56 @@ let middleName="Kumar"
 // console.log(firstName.endsWith("r"))
 
 
- console.log(fullName.trim())
+//  console.log(fullName.trim())
+
+// console.log(fullName.replace("r", "R")) //only first accurance
+// console.log(fullName.replaceAll("a","A"))
+
+//slice method in string
+//  console.log(fullName.slice(0,5))//last excluded
+
+// console.log(fullName.charAt(1))
+
+
+//split method  used transfer string to array
+
+
+// console.log(fullName.split())
+// console.log(fullName.split("")) // each chareter having comma sepreted.
+// console.log(fullName.split(" "))//split in to sentance
+// console.log(fullName.split(" ")[0])//short cut method to access index of and element
+
+
+// let token="hello wiru3947238957h8yfey752389y58h7huyr874ryijuhw7rhu"
+// console.log(token.split(" ")[1])
+
+// let name="radar"
+// let x="radar"
+// let reverse=""
+// for(let i=x.length-1; i>=0; i--){
+//     reverse=reverse+x[i]
+//     // console.log(x[i])
+
+// }
+// // console.log(reverse)
+
+// if(x==reverse){
+//     console.log("palindrome")
+// }
+// else{
+//     console.log("not palindirome")
+// }
+
+
+
+let country="nepal"
+// console.log(country.replace("n","N"))
+console.log(country[0].toUpperCase()+country.slice(1)
+)
+// console.log(country.split(""))
+
+
+
+
+
  
