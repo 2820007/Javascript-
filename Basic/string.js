69cfb81c9ecbc54ef7ab56c6a356d920985a4999
Ravi Mandal <2820007.cse@pietgroup.co.in>
2025-12-30 17:11:46 +0545

    string started

---
+++ Basic/string.js
@@ -0,0 +1,28 @@
+let fullName=" ravi mandal "
+// console.log(fullName)
+// console.log(fullName[0])
+// console.log(fullName[1])
+// for(let i=0; i<fullName.length; i++){
+//     console.log(`${i}=${fullName[i]}`)
+// }
+// for(let i=fullName.length-1; i>=0;i--){
+//     console.log(`${i}=${fullName[i]}`)
+// }
+
+
+let firstName="ravi"
+let middleName="Kumar"
+
+// console.log(firstName+ " "+ middleName)
+// console.log(firstName.concat(" " +middleName))
+// console.log(firstName.toUpperCase())
+// console.log(middleName.toLowerCase())
+
+// console.log(firstName.startsWith("r"))
+// console.log(firstName.startsWith("ravi"))
+// console.log(firstName.startsWith("R"))
+// console.log(firstName.endsWith("r"))
+
+
+ console.log(fullName.trim())
+ 
