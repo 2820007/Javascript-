444b14ec8dd9af1ef2e6c8c20e47932455ff78e7
Ravi Mandal <2820007.cse@pietgroup.co.in>
2025-12-31 17:54:22 +0545

    module completed

---
+++ Basic/module/first.js
@@ -0,0 +1,18 @@
+let x=10;
+const user={
+    name:"ravi",
+    age:33
+}
+let arr=[2,3,4,5,5]
+const hello=()=>{
+    console.log("Hello")
+}
+
+
+module.exports={
+    x,
+    user,
+    arr,
+    hello,
+    
+}
---
+++ Basic/module/second.js
@@ -0,0 +1,3 @@
+const data=require("./first.js")
+console.log(data)
+console.log(data.hello)
